ee7a8a9ca5c44116c075fcd5542c45ae52a62872
Paul <paul.audet.cain@gmail.com>
2019-02-24 12:47:10 -0500

    assignment_complete

--- scripts/album-info.js
+++ scripts/album-info.js
@@ -1,3 +1,10 @@
 {
   $('#album-title').text(album.title);
   $('img#album-cover-art').attr('src', album.albumArtUrl);
+
+  var artist = $('.artist')
+
+  artist.text(album.artist);
+
+  $('#release-info').text(album.releaseInfo);
+}
