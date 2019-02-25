e125b195a4a905a12c492dbf871d53f2e7be0f8f
Paul <paul.audet.cain@gmail.com>
2019-02-25 08:31:27 -0500

    slider_checkpoint_complete

--- scripts/player-bar.js
+++ scripts/player-bar.js
@@ -13,6 +13,7 @@
 
     const nextSong = album.songs[nextSongIndex];
     player.playPause(nextSong)
+});
 
     $('button#previous').on('click', function(){
       if(player.playState !== 'playing') { return; }
@@ -23,7 +24,18 @@
 
       const nextSong = album.songs[nextSongIndex];
       player.playPause(nextSong)
+});
 
-    })
-  });
+$('#time-control input').on('input', function (event) {
+  player.skipTo(event.target.value);
+});
+
+      setInterval( () => {
+        if (player.playState !== 'playing') {return; }
+        const currentTime = player.getTime();
+        const duration = player.getDuration();
+        const percent = (currentTime/duration) * 100 ;
+        $('#time-control .currentTime').text(currentTime);
+        $('#time-control input').val(percent);
+      },1000);
 }
